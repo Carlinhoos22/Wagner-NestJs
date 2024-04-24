a063bd38523a24eb4a37c859f3c3ff4e84bb6c25
root <weslley.c.j.4@gmail.com>
2024-04-23 22:32:10 -0300

    atualizar

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { PaymentModule } from './payment/payment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule, UsersModule, PaymentModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'blog',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+  ProductsModule],
+  //imports: [ProductsModule, UsersModule, PaymentModule],
   controllers: [AppController,],
   providers: [AppService,],
 })
