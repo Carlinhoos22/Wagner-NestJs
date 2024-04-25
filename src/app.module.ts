e20c33d23b1c72ea06713aa91eae2c5d4be6b41d
root <weslley.c.j.4@gmail.com>
2024-04-24 21:09:25 -0300

    atualizando 2.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,7 @@ import { PaymentModule } from './payment/payment.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'blog',
-    autoLoadEntities: true,
-    synchronize: true,
-  }),
-  ProductsModule],
-  //imports: [ProductsModule, UsersModule, PaymentModule],
+  imports: [ProductsModule, UsersModule, PaymentModule],
   controllers: [AppController,],
   providers: [AppService,],
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,7 +5,6 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Product } from "./entities/product.entity";
 
 @Module ({
-    imports: [TypeOrmModule.forFeature([Product])],
     controllers: [ProductsController],
     providers: [ProductsService]
 })
